8c2480a2229af2385b4e0d16bf2941943bb4c9ac
Asghar Nazir <asghar@syntaxtech.com>
2022-02-27 15:57:00 -0500

    class 16

--- src/com/syntax/class12/Calculator.java
+++ src/com/syntax/class12/Calculator.java
@@ -9,6 +9,13 @@ public class Calculator {
 		System.out.println(number1+number2);
 	}
 	
+
+	int addAndReturn() {
+		int number1=30;
+		int number2=10;
+		return number1+number2;
+	}
+	
 	void addNumbers(int number1,int number2) {
 		
 		System.out.println(number1+number2);
