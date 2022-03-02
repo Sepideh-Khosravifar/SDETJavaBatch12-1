90615efb04db1101074828eac8863f6aa4731bf0
Asghar Nazir <asghar@syntaxtech.com>
2022-03-01 22:18:29 -0500

    Class 17

--- src/com/syntax/class16/Task4.java
+++ src/com/syntax/class16/Task4.java
@@ -7,12 +7,22 @@ public class Task4 {
         reverse the words
         append the words to get the sentence back
          */
+
         String s="Sunday is great";
+
+        Task4 task4=new Task4();
+       // System.out.println(task4.reverseString(s));
         String [] strArray=s.split(" ");
-        for (int i = 0; i < strArray.length ; i++) {
-            System.out.println(strArray[i]);
+        //System.out.println(strArray[0]);
+        for (int i = 0; i < strArray.length; i++) {
+            System.out.print(task4.reverseString(strArray[i])+" ");
         }
 
-
+    }
+    String reverseString(String inputString){
+        //converting S string to StringBuilder so that we can use the reverse method
+        StringBuilder stringBuilder=new StringBuilder(inputString);
+        stringBuilder.reverse();
+        return stringBuilder.toString();
     }
 }
