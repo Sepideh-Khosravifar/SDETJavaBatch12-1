ebe96f17dcf349d05a7976d3c8e93db4827e79e0
Asghar Nazir <asghar@syntaxtech.com>
2022-03-02 22:03:45 -0500

    class 18

---
+++ src/com/syntax/class18/Facebook.java
@@ -0,0 +1,18 @@
+package com.syntax.class18;
+
+public class Facebook {
+    // break till 9:00
+    private String userName;
+    private String password;
+   private  int age;
+    void setAge(int personAge){
+        if(personAge>13&& personAge<120){
+            age=personAge;
+        }else {
+            System.out.println("Not " +
+                    "allowed its not a Horse");
+        }
+    }
+
+
+}
