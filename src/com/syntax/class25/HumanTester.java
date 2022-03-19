6dc150b37bbac25937771e1505e82141adbd4e82
Asghar Nazir <asghar@syntaxtech.com>
2022-03-19 15:09:31 -0400

    Class 25

---
+++ src/com/syntax/class25/HumanTester.java
@@ -0,0 +1,17 @@
+package com.syntax.class25;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HumanTester {
+    public static void main(String[] args) {
+        Maha maha=new Maha();
+        maha.walk();
+        //  Maha.printLegs();
+        Human.printLegs();
+        LivingBeing.printLegs();
+        WebDriver webDriver=new FirefoxDriver();
+        webDriver.get("www.google.com");
+    }
+}
