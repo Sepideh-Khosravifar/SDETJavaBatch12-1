65ab86c9952935fb93ca4f19a4c79d53b8dea2e2
Asghar Nazir <asghar@syntaxtech.com>
2022-03-28 20:26:03 -0400

    Review class 10

---
+++ src/com/syntax/reviewclass10/Human.java
@@ -0,0 +1,18 @@
+package com.syntax.reviewclass10;
+
+public class Human{
+    private String name;
+    private String color;
+    private int age;
+
+    Human(String name, String color,int age){
+      this.name=name;
+      this.color=color;
+      this.age=age;
+        System.out.println("Inside the parent class Human ");
+    }
+
+}
+class Employee{
+
+}
